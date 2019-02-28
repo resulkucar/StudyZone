420a307b4fbc1145921578aee4fcac9c760caeb8
Resul <rucar@purdue.edu>
2019-02-27 19:49:54 -0500

    Finalized logo and added 3 sec transition

--- app/src/main/java/com/example/studyzone/MainActivity.java
+++ app/src/main/java/com/example/studyzone/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.studyzone;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        setContentView(R.layout.activity_main);
+        //This gives that 3 second delay when you first open it
+        new Timer().schedule(new TimerTask() {
+            public void run() {
+                startActivity(new Intent(MainActivity.this, Statistics.class));
+                finish();
+
+            }
+        }, 3000);
     }
 }
