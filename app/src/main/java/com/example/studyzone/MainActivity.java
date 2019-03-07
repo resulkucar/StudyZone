0da3182e0a38aa471abe98bb7fd482a12840bd61
Alexander Kashyap <alexander.kashyap@gmail.com>
2019-03-06 20:52:13 -0500

    Finished todo list code, test working?
    first attempt at finished todo list, will move around xml garbage once it starts to actually work.

--- app/src/main/java/com/example/studyzone/MainActivity.java
+++ app/src/main/java/com/example/studyzone/MainActivity.java
@@ -3,14 +3,16 @@ package com.example.studyzone;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
     private EditText itemET;
     private Button btn;
     private ListView itemsList;
@@ -24,16 +26,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         itemET = findViewById(R.id.item_edit_text);
         btn = findViewById(R.id.add_btn);
         itemsList = findViewById(R.id.items_list);
-
+        items = FileHelper.readData(this);
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,items);
+        itemsList.setAdapter(adapter);
         btn.setOnClickListener(this);
-
+        itemsList.setOnItemClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
+        switch(v.getId()){
 
+            case R.id.add_btn:
+                String itemEntered = itemET.getText().toString();
+                adapter.add(itemEntered);
+                itemET.setText("");
+                FileHelper.writeData(items,this);
+
+                Toast.makeText(this, "Item Added", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        items.remove(position);
+        adapter.notifyDataSetChanged();
+        FileHelper.writeData(items, this);
+        Toast.makeText(this, "Deleted", Toast.LENGTH_SHORT).show();
+    }
 }
