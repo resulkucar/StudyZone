98b95e57ce2d2b3ef801035187a8096efc557b88
Roop Patel <46769476+Roop175@users.noreply.github.com>
2019-04-15 12:11:57 -0400

    aaa

---
+++ app/src/main/java/com/example/sign_in/pickActivity.java
@@ -0,0 +1,56 @@
+package com.example.sign_in;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class pickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick);
+
+        findViewById(R.id.addBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(pickActivity.this, endDate.class);
+                startActivity(intent);
+            }
+        });
+
+
+        findViewById(R.id.removeBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(pickActivity.this, remList.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.musicBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(pickActivity.this, playlistPicker.class);
+                startActivity(intent);
+            }
+        });
+
+//        findViewById(R.id.homeBtn).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(pickActivity.this, HomeActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        findViewById(R.id.listBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(pickActivity.this, todo.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/sign_in/startTime.java
@@ -0,0 +1,46 @@
+package com.example.sign_in;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TimePicker;
+
+public class startTime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_time);
+
+        Intent in = getIntent();
+        Bundle b = in.getExtras();
+        final int year = (int) b.get("year");
+        final int month = (int) b.get("month");
+        final int date = (int) b.get("date");
+        final String day = (String) b.get("day");
+
+        final TimePicker timePicker = findViewById(R.id.startTime);
+
+        findViewById(R.id.confBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int hour = timePicker.getHour();
+                final int minute = timePicker.getMinute();
+                final String time = Integer.toString(hour) + ":" + Integer.toString(minute);
+
+                Intent intent = new Intent(startTime.this, ConfActivity.class);
+                intent.putExtra("time", time);
+                intent.putExtra("hour", hour);
+                intent.putExtra("minute", minute);
+                intent.putExtra("year", year);
+                intent.putExtra("month", month);
+                intent.putExtra("date", date);
+                intent.putExtra("day", day);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+}
